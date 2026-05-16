17db2b802ada4d4925751a66a09ef06b3d2928bb
whysamay <samaypawar2200@gmail.com>
2026-05-16 14:30:12 +0530

    Local mlflow ui code

---
+++ src/exp_mlflow_local.py
@@ -0,0 +1,66 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#run this in another terminal
+#mlflow server --host 127.0.0.1 --port 5000
+
+#local URL where we can find MLflow UI
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Load Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 10
+n_estimators = 5
+
+# Mention your experiment below
+mlflow.set_experiment('Exp_1')
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    #logging metrics    
+    mlflow.log_metric('accuracy', accuracy)
+
+    #logging params
+    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
+
+    # Creating a confusion matrix plot
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    # save plot
+    plt.savefig("Confusion-matrix.png")
+
+    # log artifacts using mlflow
+    mlflow.log_artifact("Confusion-matrix.png")
+    mlflow.log_artifact(__file__)
+
+    # tags
+    mlflow.set_tags({"Author": 'Samay', "Project": "Iris Classification"})
+
+    # Log the model
+    mlflow.sklearn.log_model(rf, "Random-Forest-Model")
+
+    print(accuracy)
